e58533a49857a2104ba85722e36e98abf96f074b
twells46 <tom.wells46@protonmail.com>
2024-11-02 22:21:52 -0500

    Sql query no chapters

--- main.go
+++ main.go
@@ -1,14 +1,117 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+	"time"
 
-	md "github.com/twells46/gomangatool/internal/mdapi"
+	_ "github.com/mattn/go-sqlite3"
 )
 
+type Tag struct {
+	TagID    int
+	TagTitle string
+}
+
+type Chapter struct {
+	ChapterHash string
+	ChapterNum  float64
+	MangaID     string
+	ChapterName string
+	Download    bool
+	IsRead      bool
+}
+
+type Manga struct {
+	MangaID      string
+	SerTitle     string
+	FullTitle    string
+	Descr        string
+	TimeModified time.Time
+	Tags         []Tag
+	Chapters     []Chapter
+}
+
+type SQLite struct {
+	db *sql.DB
+}
+
+type Database interface {
+	Init() error
+	Create(manga Manga) error
+	GetAll() ([]Manga, error)
+	Get(id string) (*Manga, error)
+}
+
+func NewDb(db *sql.DB) *SQLite {
+	return &SQLite{db: db}
+}
+
+func (r *SQLite) GetChapters(MangaID string) []Chapter {
+	query := `
+	SELECT *
+	FROM Chapter
+	WHERE MangaID = ?`
+	rows, err := r.db.Query(query, MangaID)
+	if err != nil {
+		log.Fatalf("%s: Failed to query db", err)
+	}
+	rows.Close()
+
+	var all []Chapter
+	for rows.Next() {
+		var c Chapter
+		err := rows.Scan(&c.ChapterHash, &c.ChapterNum, &c.MangaID, &c.ChapterName, &c.Download, &c.IsRead)
+		if err != nil {
+			log.Fatalf("%s: Failed to query db", err)
+		}
+		all = append(all, c)
+	}
+
+	return all
+}
+
+func (r *SQLite) GetAll() ([]Manga, error) {
+	rows, err := r.db.Query("SELECT * FROM Manga")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var all []Manga
+
+	for rows.Next() {
+		var m Manga
+		err := rows.Scan(&m.MangaID, &m.SerTitle, &m.FullTitle, &m.Descr, &m.TimeModified)
+		if err != nil {
+			return nil, err
+		}
+		m.Chapters = r.GetChapters(m.MangaID)
+		all = append(all, m)
+	}
+
+	return all, nil
+}
+
 func main() {
-	//md.DlChapter(`362936f9-2456-4120-9bea-b247df21d0bc`)
-	feed := md.GetFeed(`6941f16b-b56e-404a-b4ba-2fc7e009d38f`, 0)
-	fmt.Println(feed)
-	fmt.Println("asdf")
+	/*
+		md.DlChapter(`362936f9-2456-4120-9bea-b247df21d0bc`)
+		feed := md.GetFeed(`6941f16b-b56e-404a-b4ba-2fc7e009d38f`, 0)
+		fmt.Println(feed)
+	*/
+	db, err := sql.Open("sqlite3", "test.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	store := NewDb(db)
+
+	test, err := store.GetAll()
+	if err != nil {
+		log.Fatalf("%s: Failed to query db", err)
+	}
+
+	fmt.Println(test)
 }
--- reference.sql
+++ reference.sql
@@ -4,7 +4,7 @@ CREATE TABLE Manga(
     SerTitle VARCHAR(32) NOT NULL UNIQUE,
     FullTitle VARCHAR(128) NOT NULL,
     Descr VARCHAR(1024),
-    TimeModified INTEGER
+    TimeModified DATETIME
 );
 
 CREATE TABLE Tag (
@@ -35,9 +35,9 @@ CREATE TABLE Chapter (
     FOREIGN KEY (MangaID) REFERENCES Manga(MangaID)
 );
 
-INSERT INTO Tag (TagTitle) VALUES ('romance');
-INSERT INTO Tag (TagTitle) VALUES ('harem');
-INSERT INTO Manga VALUES('ee51d8fb-ba27-46a5-b204-d565ea1b11aa', 'kokuhaku_sarete', 'asdf', 'The servant of the Tendou family, Eito, serves the perfect young lady, Hoshine. One day, Eito informs Hoshine that someone from another class confessed their feelings for him. Hoshine, who has hidden her feelings for Eito since childhood, begins to feel uneasy: «I''ve loved Eito for a much longer time!» As a result, Hoshine starts to approach Eito even more, making bolder advances than ever before! She gets close to him in crowded trains and begs him to sleep by her side… While Eito tries to maintain his role as a formal servant, Hoshine increasingly pursues him with her advances. It''s an adorable romantic comedy between a mistress and her servant, featuring a young lady who strives to win her servant''s love!', 0);
+INSERT INTO Tag (TagTitle) VALUES ('Romance');
+INSERT INTO Tag (TagTitle) VALUES ('Harem');
+INSERT INTO Manga VALUES('ee51d8fb-ba27-46a5-b204-d565ea1b11aa', 'kokuhaku_sarete', 'Ore ga Kokuhaku Sarete Kara, Ojou no Yousu ga Okashii', 'The servant of the Tendou family, Eito, serves the perfect young lady, Hoshine. One day, Eito informs Hoshine that someone from another class confessed their feelings for him. Hoshine, who has hidden her feelings for Eito since childhood, begins to feel uneasy: «I''ve loved Eito for a much longer time!» As a result, Hoshine starts to approach Eito even more, making bolder advances than ever before! She gets close to him in crowded trains and begs him to sleep by her side… While Eito tries to maintain his role as a formal servant, Hoshine increasingly pursues him with her advances. It''s an adorable romantic comedy between a mistress and her servant, featuring a young lady who strives to win her servant''s love!', 0);
 INSERT INTO Manga VALUES('6941f16b-b56e-404a-b4ba-2fc7e009d38f', 'assistant_for_life', 'asdf', 'asdf', 12345);
 
 INSERT INTO Chapter VALUES('598c7824-5822-4ac0-90f5-5439f1f7015e', 1.1, 'ee51d8fb-ba27-46a5-b204-d565ea1b11aa', 'Ch. 1.1', 1, 1);

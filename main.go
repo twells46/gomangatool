01df91534919e8369c9dcfa62e70ddfb491543ea
twells46 <tom.wells46@protonmail.com>
2024-11-24 11:26:01 -0600

    cleanup

--- main.go
+++ main.go
@@ -15,26 +15,4 @@ func main() {
 	if _, err := p.Run(); err != nil {
 		log.Fatalln(err)
 	}
-
-	/*
-		tester := all[0]
-			tester.Chapters = backend.DownloadAll(tester.Chapters, store)
-			fmt.Println(tester)
-	*/
-	//new := backend.RefreshFeed(tester, store)
-	//fmt.Println(new)
-
-	//fmt.Println(manga)
-	//store := backend.Opendb("manga.sqlite3")
-	//backend.NewManga(seriesID, store)
-	/*
-		log.SetFlags(log.Lshortfile | log.LstdFlags)
-		md.DlChapter(`362936f9-2456-4120-9bea-b247df21d0bc`)
-		feed := md.GetFeed(`6941f16b-b56e-404a-b4ba-2fc7e009d38f`, 0)
-		fmt.Println(feed)
-
-		store := backend.Opendb("manga.sqlite3")
-		//backend.SqlTester()
-		backend.NewManga("ee51d8fb-ba27-46a5-b204-d565ea1b11aa", store)
-	*/
 }
